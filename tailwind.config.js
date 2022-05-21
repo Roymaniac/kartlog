00d72014928c14ebdca7beb852c31fc5b6305cd1
Roymaniac <amolojoseph2020@gmail.com>
2022-05-21 14:43:41 +0100

    web setup

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg:'976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        lightGray: 'rgba(0, 0, 0, 0.7)',
+        lightBlue: '#0777A1',
+        lightAsh: '#EAEAEA',
+        countPink: '#FF7990'
+      },
+      fontFamily: {
+        heading: ['Poppins', 'sans-serif']
+      }
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: "",
+    themes: false,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
+}

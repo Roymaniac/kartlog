7593dca233b50b8b194288d1eb01ebdb91c1fa6a
Amolo Chukwudumebi  Joseph <65375943+Roymaniac@users.noreply.github.com>
2023-02-02 14:44:24 +0100

    updated count down timer

--- js/count.js
+++ js/count.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("August 5, 2022 12:00:00").getTime();
+var countDownDate = new Date("August 5, 2023 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
